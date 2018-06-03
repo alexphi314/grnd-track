845d5b34631b0c5cee9e5d67b5888d1463eefadb
Alex Philpott <alex_philpott@icloud.com>
2018-06-03 10:06:41 -0600

    Updated git ignore and added plotting of starting/ending points on ground track

--- plot_track.m
+++ plot_track.m
@@ -9,7 +9,10 @@ load coastlines;
 plotm(coastlat,coastlon);
 
 if length(inputLong) > 0 && length(inputLat) > 0
-    plotm(inputLat,inputLong,'*k');
+    plotm(inputLat,inputLong,'k');
+    p1 = plotm(inputLat(1),inputLong(1),'ob','DisplayName','Starting Position');
+    p2 = plotm(inputLat(end),inputLong(end),'or','DisplayName','Ending Position');
+    legend([p1,p2]);
 end
 
 print('ground_track.jpg','-djpeg');
